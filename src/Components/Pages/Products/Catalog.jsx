086c2ec0c017814adb7d413e499fbb7a6f12336c
lauttigl <ljiglesias@outlook.com>
2023-01-27 20:55:14 -0300

    agregue products y estilos

--- src/Components/Pages/Products/Catalog.jsx
+++ src/Components/Pages/Products/Catalog.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { products } from '../../../DataBase/Database'
 
 export const Catalog = () => {
+
+  
+
   return (
-    <div>Catalog</div>
-  )
-}
+    <div className="bg-stone-800">
+    <div className="card lg:card-side card-bordered space-y-4 bg-stone-800 shadow-xl gap-2 ">
+      {products.map((product) => (
+        <div key={product.id}>
+        <figure><img src={product.imageSrc} alt="Album"/></figure>
+  <div className="card-body  ">
+    <h2 className="card-title justify-center text-zinc-300">{product.name}</h2>
+    <p className="text-zinc-300 text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat harum aliquam nobis debitis. Voluptatibus, iusto?</p>
+    <p className='text-zinc-300'>Precio: ${product.price}</p>
+    <div className="card-actions justify-center">
+      <button className="btn bg-red-500 hover:bg-black rounded-full p-4 m-2 font-bold text-zinc-300">Ver Producto</button>
+    </div>  
+  </div>
+  </div>
+  ))}
+</div>
+</div>
+  )}
